75884de5c964b73ae1d77950930f6d9f33fe51bd
Vitalii Halkin <galkin.vitaly@gmail.com>
2017-08-10 14:20:11 +0300

    Add testing for PaginatedBulkLoader class

--- scraper/tests.py
+++ scraper/tests.py
@@ -1,10 +1,11 @@
 import requests
 import itertools
+
 from django.test import TestCase, mock
 from model_mommy import mommy
 
 from scraper.models import School, Department
-from scraper.management.commands.scrape_data import BulkLoader, Url
+from scraper.utils import BulkLoader, Url, PaginatedBulkLoader
 
 
 def mocked_requests_get(*args, **kwargs):
@@ -94,6 +95,254 @@ def mocked_requests_get(*args, **kwargs):
         return MockResponse({}, 200)
     elif args[0] == 'https://www.myedu.com/adms/school/3/department/':
         raise requests.ConnectionError
+    elif args[0] == 'https://www.myedu.com/adms/school/paginated?page=1':
+        return MockResponse({
+            'status': 200,
+            'message': 'OK',
+            'result': {},
+            'pagination': {
+                'results': 10,
+                'page': 1,
+                'pages': 2,
+                'per_page': 5
+            }
+        }, 200)
+    elif args[0] == 'https://www.myedu.com/adms/school/notpaginated?page=1':
+        return MockResponse({
+            'status': 200,
+            'message': 'OK',
+            'result': {},
+        }, 200)
+    elif args[0] == 'https://www.myedu.com/adms/school/?page=1' or args[0] == 'https://www.myedu.com/adms/school/?page=2':
+        return MockResponse({
+            'status': 200,
+            'message': 'OK',
+            'result': {
+                'School': {
+                    2: {
+                        'UID': 2,
+                        'abbreviation': 'UHV ',
+                        'address': '3007 N. Ben Wilson',
+                        'active': True,
+                        'campus_setting': 'City: Small',
+                        'city': 'Victoria',
+                        'color': '333571',
+                        'mascot': 'Jaguars',
+                        'common_name': 'UH-Victoria',
+                        'datasource_code': 'EXP',
+                        'datasource_id': 1,
+                        'datasource_user_id': 15,
+                        'edu_time': '4-year',
+                        'edu_type': 'Public',
+                        'education_network_member': None,
+                        'has_grades': True,
+                        'graduate_population': 1806,
+                        'launch_group_code': 'AUG',
+                        'launch_group_id': 4,
+                        'launch_priority': 593,
+                        'launch_status_code': 'LIVE',
+                        'launch_status_id': 6,
+                        'on_campus_avail': True,
+                        'no_registration': None,
+                        'parent_school_id': None,
+                        'revenue': None,
+                        'school_bucket': '1',
+                        'school_id': 2,
+                        'school_name': 'University of Houston-Victoria',
+                        'school_seo_title': None,
+                        'school_seo_url': '/UHV-University-of-Houston-Victoria/school/2',
+                        'school_seo_description': None,
+                        'course_by_dept_seo_url': '/UHV-University-of-Houston-Victoria/school/2/course/by-department/',
+                        'degree_by_college_seo_url': '/UHV-University-of-Houston-Victoria/school/2/degree/by-college/',
+                        'prof_by_dept_seo_url': '/UHV-University-of-Houston-Victoria/school/2/professor/by-department/',
+                        'school_type': 'Undergraduate',
+                        'state': 'TX',
+                        'undergraduate_population': 2289,
+                        'zip': '77901',
+                        'registration_url': None,
+                        'degree_audit_url': None
+                    },
+                    8: {
+                        'UID': 8,
+                        'abbreviation': None,
+                        'address': '100 State Sreet',
+                        'active': None,
+                        'campus_setting': 'City: Small',
+                        'city': 'Framingham',
+                        'color': '505050',
+                        'mascot': 'Rams',
+                        'common_name': None,
+                        'datasource_code': 'EXP',
+                        'datasource_id': 1,
+                        'datasource_user_id': 15,
+                        'edu_time': '4-year',
+                        'edu_type': 'Public',
+                        'education_network_member': None,
+                        'has_grades': False,
+                        'graduate_population': 2095,
+                        'launch_group_code': 'BETA',
+                        'launch_group_id': 1,
+                        'launch_priority': 1158,
+                        'launch_status_code': 'NEW',
+                        'launch_status_id': 1,
+                        'on_campus_avail': True,
+                        'no_registration': None,
+                        'parent_school_id': None,
+                        'revenue': None,
+                        'school_bucket': None,
+                        'school_id': 8,
+                        'school_name': 'Framingham State University',
+                        'school_seo_title': None,
+                        'school_seo_url': '/Framingham-State-University/school/8',
+                        'school_seo_description': None,
+                        'course_by_dept_seo_url': '/Framingham-State-University/school/8/course/by-department/',
+                        'degree_by_college_seo_url': '/Framingham-State-University/school/8/degree/by-college/',
+                        'prof_by_dept_seo_url': '/Framingham-State-University/school/8/professor/by-department/',
+                        'school_type': 'Undergraduate',
+                        'state': 'MA',
+                        'undergraduate_population': 3858,
+                        'zip': '01701-9101',
+                        'registration_url': None,
+                        'degree_audit_url': None
+                    },
+                    10: {
+                        'UID': 10,
+                        'abbreviation': None,
+                        'address': '200 College Rd',
+                        'active': None,
+                        'campus_setting': 'Rural: Fringe',
+                        'city': 'Gallup',
+                        'color': '505050',
+                        'mascot': None,
+                        'common_name': None,
+                        'datasource_code': 'EXP',
+                        'datasource_id': 1,
+                        'datasource_user_id': 15,
+                        'edu_time': '2-year',
+                        'edu_type': 'Public',
+                        'education_network_member': None,
+                        'has_grades': False,
+                        'graduate_population': None,
+                        'launch_group_code': None,
+                        'launch_group_id': None,
+                        'launch_priority': 1940,
+                        'launch_status_code': 'NEW',
+                        'launch_status_id': 1,
+                        'on_campus_avail': None,
+                        'no_registration': True,
+                        'parent_school_id': None,
+                        'revenue': None,
+                        'school_bucket': None,
+                        'school_id': 10,
+                        'school_name': 'University of New Mexico-Gallup Campus',
+                        'school_seo_title': None,
+                        'school_seo_url': '/University-of-New-Mexico-Gallup-Campus/school/10',
+                        'school_seo_description': None,
+                        'course_by_dept_seo_url': '/University-of-New-Mexico-Gallup-Campus/school/10/course/by-department/',
+                        'degree_by_college_seo_url': '/University-of-New-Mexico-Gallup-Campus/school/10/degree/by-college/',
+                        'prof_by_dept_seo_url': '/University-of-New-Mexico-Gallup-Campus/school/10/professor/by-department/',
+                        'school_type': 'Undergraduate',
+                        'state': 'NM',
+                        'undergraduate_population': 3010,
+                        'zip': '87301-5697',
+                        'registration_url': None,
+                        'degree_audit_url': None
+                    },
+                    11: {
+                        'UID': 11,
+                        'abbreviation': None,
+                        'address': '6001 Dodge Street ',
+                        'active': None,
+                        'campus_setting': 'None',
+                        'city': 'Omaha',
+                        'color': '505050',
+                        'mascot': None,
+                        'common_name': None,
+                        'datasource_code': 'EXP',
+                        'datasource_id': 1,
+                        'datasource_user_id': 15,
+                        'edu_time': 'unknown',
+                        'edu_type': 'unknown',
+                        'education_network_member': None,
+                        'has_grades': False,
+                        'graduate_population': None,
+                        'launch_group_code': None,
+                        'launch_group_id': None,
+                        'launch_priority': 6394,
+                        'launch_status_code': 'NEW',
+                        'launch_status_id': 1,
+                        'on_campus_avail': None,
+                        'no_registration': None,
+                        'parent_school_id': None,
+                        'revenue': None,
+                        'school_bucket': None,
+                        'school_id': 11,
+                        'school_name': 'University of Nebraska, Omaha',
+                        'school_seo_title': None,
+                        'school_seo_url': '/University-of-Nebraska-Omaha/school/11',
+                        'school_seo_description': None,
+                        'course_by_dept_seo_url': '/University-of-Nebraska-Omaha/school/11/course/by-department/',
+                        'degree_by_college_seo_url': '/University-of-Nebraska-Omaha/school/11/degree/by-college/',
+                        'prof_by_dept_seo_url': '/University-of-Nebraska-Omaha/school/11/professor/by-department/',
+                        'school_type': 'Undergraduate',
+                        'state': 'NE',
+                        'undergraduate_population': None,
+                        'zip': '68182',
+                        'registration_url': None,
+                        'degree_audit_url': None
+                    },
+                    12: {
+                        'UID': 12,
+                        'abbreviation': None,
+                        'address': '3300 Century Ave N',
+                        'active': None,
+                        'campus_setting': 'Suburb: Large',
+                        'city': 'White Bear Lake',
+                        'color': '505050',
+                        'mascot': None,
+                        'common_name': None,
+                        'datasource_code': 'EXP',
+                        'datasource_id': 1,
+                        'datasource_user_id': 15,
+                        'edu_time': '2-year',
+                        'edu_type': 'Public',
+                        'education_network_member': None,
+                        'has_grades': False,
+                        'graduate_population': None,
+                        'launch_group_code': None,
+                        'launch_group_id': None,
+                        'launch_priority': 1013,
+                        'launch_status_code': 'NEW',
+                        'launch_status_id': 1,
+                        'on_campus_avail': None,
+                        'no_registration': None,
+                        'parent_school_id': None,
+                        'revenue': None,
+                        'school_bucket': None,
+                        'school_id': 12,
+                        'school_name': 'Century Community and Technical College',
+                        'school_seo_title': None,
+                        'school_seo_url': '/Century-Community-and-Technical-College/school/12',
+                        'school_seo_description': None,
+                        'course_by_dept_seo_url': '/Century-Community-and-Technical-College/school/12/course/by-department/',
+                        'degree_by_college_seo_url': '/Century-Community-and-Technical-College/school/12/degree/by-college/',
+                        'prof_by_dept_seo_url': '/Century-Community-and-Technical-College/school/12/professor/by-department/',
+                        'school_type': 'Undergraduate',
+                        'state': 'MN',
+                        'undergraduate_population': 10918,
+                        'zip': '55110',
+                        'registration_url': None,
+                        'degree_audit_url': None
+                    }
+                }
+            },
+            'pagination': {
+                'results': 10,
+                'page': 1,
+                'pages': 2,
+                'per_page': 5
+            }
+        }, 200)
 
     return MockResponse(None, 404)
 
@@ -217,3 +466,44 @@ class BulkLoaderTest(TestCase):
         self.assertFalse(self.tbl.work_with_db)
         self.assertFalse(self.tbl.success_ids)
         self.assertFalse(self.tbl.save_list)
+
+
+class PaginatedBulkLoaderTest(TestCase):
+    def setUp(self):
+        self.tbl = PaginatedBulkLoader()
+
+    def tearDown(self):
+        School.objects.all().delete()
+        # todo think about testability while refactoring
+        # this is needed cause I have to update this class attributes for proper testing
+        PaginatedBulkLoader.pages = 1
+        self.tbl = None
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_paginated(self, mock_get):
+        paginated = 'https://www.myedu.com/adms/school/paginated'
+        self.assertEqual(self.tbl.pages, 1)
+        self.assertTrue(self.tbl.is_paginated(url=paginated))
+        self.assertEqual(self.tbl.pages, 2)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_not_paginated(self, mock_get):
+        not_paginated = 'https://www.myedu.com/adms/school/notpaginated'
+        self.assertEqual(self.tbl.pages, 1)
+        self.assertFalse(self.tbl.is_paginated(url=not_paginated))
+        self.assertEqual(self.tbl.pages, 1)
+
+    def test_get_urls(self):
+        urls = list(self.tbl.get_urls())
+        self.assertEqual(len(urls), self.tbl.pages)
+        for url in urls:
+            self.assertIsInstance(url, Url)
+            self.assertIn('page', url.url)
+            self.assertRegex(url.url, 'https://www.myedu.com/adms/school/\?page=[\d+]')
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_process_urls(self, mock_get):
+        self.assertFalse(School.objects.exists())
+        self.tbl.process_urls()
+        self.assertTrue(School.objects.exists())
+        self.assertEqual(School.objects.count(), 10)
---
+++ scraper/utils.py
@@ -0,0 +1,321 @@
+import gc
+import logging
+import requests
+import time
+from queue import Queue
+from threading import Thread
+
+from django.core.paginator import Paginator
+from django.db import transaction
+
+from scraper.models import School, Department
+
+
+API_URL = 'https://www.myedu.com/adms'
+SCHOOL_PATH = '/school/'
+DEPARTMENT_PATH = '/school/{}/department/'
+COURSE_PATH = '/department/{department_id}/course/'
+PROFESSOR_PATH = '/department/{department_id}/professor/'
+
+
+class LoggingMixin(object):
+    _disable_logging = False
+    _logger = None
+
+    def set_logger(self, logger=None):
+        # logger argument should be result of logging.getLogger(name) call
+        if logger:
+            self._logger = logger
+        else:
+            # Logger was not provided. Continue without logging
+            self._disable_logging = True
+
+    def log(self, msg, level=logging.INFO):
+        # todo get caller info (class, function, thread or process)
+        # todo use self.stdout.write or print if logger is not available
+        if self._logger and not self._disable_logging:
+            self._logger.log(level, msg)
+
+
+class ProcessBigAmountsMixin(LoggingMixin):
+    chunksize = 1000
+    chunktimeout = 0
+    model = None
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def queryset_iterator(self, queryset):
+        count = queryset.count()
+        self.log('Process ({})'.format(count))
+        if not count > 0:
+            self.log('0 objects processed')
+            raise StopIteration
+
+        start_index = 1
+        # eliminate problem for queryset laziness when changing queryset's filter criteria in object
+        # example
+        # from school.models import School
+        # School.objects.filter(is_saved=False).count()
+        # if you update flag is_saved for every chunk queryset will be halved
+        # understand better queryset laziness trying to optimize this
+        # and look here for reference https://stackoverflow.com/questions/10548744/django-lazy-queryset-and-pagination
+        ids = list(queryset.values_list('id', flat=True))
+        paginator = Paginator(ids, self.chunksize)
+        # paginator = Paginator(queryset, self.chunksize)
+        page = paginator.page(1)
+        has_next_chunk = True
+        while has_next_chunk:
+            obj_instances = queryset.model.objects.filter(id__in=page.object_list)
+            # obj_instances = page.object_list
+            with transaction.atomic():
+                for index, row in enumerate(obj_instances, start_index):
+                    yield row
+
+            start_index = index + 1
+            gc.collect()
+            if page.has_next():
+                self.log('{} processed from {}'.format(self.chunksize * page.number, count))
+                page = paginator.page(page.next_page_number())
+                time.sleep(self.chunktimeout)
+            else:
+                has_next_chunk = False
+
+
+# todo divide BulkLoader to Executor, DataGetter and DataSaver classes
+class BulkLoader(LoggingMixin):
+    url_template = '{api_url}{obj_path}'.format(api_url=API_URL, obj_path=DEPARTMENT_PATH)
+    # string for traversing json objects
+    key = 'result.Department'
+    req_count = 0
+    max_req_count = 300
+    save_count = 100
+    success_ids = []
+    save_list = []
+    save_class = Department
+    fetch_class = School
+    work_with_db = False
+    _error = False
+
+    # todo add logger argument which could be string or actual logger
+    def __init__(self, logger=None):
+        super(BulkLoader, self).__init__()
+        self.set_logger(logger=logger)
+
+    @staticmethod
+    def get_response(url):
+        response = requests.get(url, timeout=10)
+        if response.ok:
+            return response.json()
+        else:
+            response.raise_for_status()
+
+    def get_objects_from_url(self, data):
+        """Uses self.key to traverse json result"""
+        for ki in self.key.split('.'):
+            try:
+                data = data[ki]
+            except (AttributeError, KeyError) as e:
+                result = {}
+            else:
+                result = data
+        return result
+
+    def get_values_queryset(self):
+        return self.fetch_class.objects.filter(department_scraped=False).values_list('school_id', flat=True)
+
+    def get_urls(self):
+        values_queryset = self.get_values_queryset()
+        for v in values_queryset:
+            query_url = self.url_template.format(v)
+            # todo move this id propagation to a dict where url will be the key and propagated id on of the values
+            yield Url(query_url, id_to_update=v)
+
+    def process_url(self, url):
+        """Expects Url instance to process"""
+
+        self._error = False
+        # todo use try except else instead of _error = True
+        # todo move this to process_url method
+        try:
+            resp_data = self.get_response(url.url)
+        except (KeyboardInterrupt, SystemExit):
+            self.log('Going to halt now', level=logging.WARNING)
+            # http://effbot.org/zone/stupid-exceptions-keyboardinterrupt.html
+            raise
+        # maybe add division by requests exceptions (Timeout, HTTPError, ConnectionError)
+        except requests.RequestException:
+            # todo add handle_error method
+            # todo check error reporting when disconnected
+            self.log('Request exception: {}'.format(url.url), level=logging.ERROR)
+            self._error = True
+            objs = {}
+        except:
+            # todo propagate exc_info=True
+            # https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
+            self.log('Unexpected error: {}'.format(url.url), level=logging.ERROR)
+            # print("Unexpected error:", sys.exc_info()[0])
+            self._error = True
+            objs = {}
+        else:
+            objs = self.get_objects_from_url(resp_data)
+
+        # todo move to save_objs method after refactoring and decoupling
+        for key in objs.keys():
+            obj = objs[key]
+            # todo think better name for url
+            obj.update({'source_url': url.url})
+            # add sleep here. Should wait with list population while updating db
+            #  or maybe this is not a problem
+            # Should i use queues instead of lists?
+            # add subtle logging to check if this is a problem especially in thread mode
+            # todo rewrite with conditional variables or events and make all alive threads wait until work with db is not complete
+            while self.work_with_db:
+                time.sleep(1)
+            # todo think if save_list could be a bunch of url objects not actual class to save,
+            #  so I can remove success_ids list
+            self.save_list.append(self.save_class(**obj))
+            # add to success dict for bulk update via method final_update
+            # if url.id_to_update:
+            #     self.success_ids.append(url.id_to_update)
+
+        # url without objects in result list should be marked as processed
+        if not self._error:
+            while self.work_with_db:
+                time.sleep(1)
+            # add to success dict for bulk update via method final_update
+            if url.id_to_update:
+                self.success_ids.append(url.id_to_update)
+
+    def process_urls(self):
+        self.log('Start data loading')
+        for url in self.get_urls():
+
+            self.process_url(url)
+
+            if self.req_count % self.save_count == 0:
+                self.update_db()
+                self.log('Process request {}'.format(self.req_count))
+
+            self.req_count += 1
+
+            if self.req_count == self.max_req_count:
+                self.log('Hit max request at {}'.format(self.req_count))
+                break
+        self.update_db()
+        self.log('Finish data loading')
+
+    def update_db(self):
+        # todo add transaction here and halt execution in case of any problems
+        # understand could be a problem here when adding new values to this lists while db queries
+        # try switch to use queue
+        # https://stackoverflow.com/questions/6319207/are-lists-thread-safe
+        self.work_with_db = True
+        School.objects.filter(school_id__in=self.success_ids).update(department_scraped=True)
+        self.save_class.objects.bulk_create(objs=self.save_list)
+        self.save_list = []
+        self.success_ids = []
+        self.work_with_db = False
+
+
+class Url(object):
+    """helper class used to store all necessary data about urls that should be processed"""
+    processed = False
+
+    def __init__(self, url, id_to_update=None):
+        self.url = url
+        self.id_to_update = id_to_update
+
+
+class ThreadedBulkLoader(BulkLoader):
+    # todo add as argument to initialization of class
+    concurrent = 5
+    # multiplier for queue size
+    _concurrent_multiplier = 3
+    max_req_count = 15000
+
+    def __init__(self, logger=None):
+        super(ThreadedBulkLoader, self).__init__(logger=logger)
+        self.q = Queue(self.concurrent * self._concurrent_multiplier)
+
+    def process_urls(self):
+
+        def worker():
+            while True:
+                item = self.q.get()
+                # todo make process url method to add processed url to another queue that will be consumed by single process or thread
+                self.process_url(item)
+                self.q.task_done()
+                # todo add acquire lock here
+                self.req_count += 1
+
+        for i in range(self.concurrent):
+            t = Thread(target=worker)
+            t.daemon = True
+            t.start()
+
+        self.log('Start data loading')
+        for url in self.get_urls():
+            self.q.put(url)
+
+            # todo think where this counters should be
+            if self.req_count > 0 and self.req_count % self.save_count == 0:
+                self.update_db()
+                self.log('Process request {}'.format(self.req_count))
+
+            if self.req_count == self.max_req_count:
+                self.log('Hit max request at {}'.format(self.req_count))
+                break
+        self.q.join()
+        # if this is before join some small amount of objects wasn't saved
+        self.update_db()
+        self.log('Finish data loading')
+
+
+class PaginatedBulkLoader(BulkLoader):
+    key = 'result.School'
+    url_template = '{api_url}{obj_path}'.format(api_url=API_URL, obj_path=SCHOOL_PATH)
+    save_class = School
+    pages = 1
+    max_req_count = 2000
+    save_count = 100
+
+    def is_paginated(self, url):
+        paged_url = '{url}{query}'.format(url=url, query='?page=1')
+        # todo add logging and error handling here
+        resp_result = self.get_response(paged_url)
+        try:
+            pag_dict = resp_result['pagination']
+        except KeyError:
+            return False
+        else:
+            self.pages = pag_dict['pages']
+            return True
+
+    def get_urls(self):
+        query_url = self.url_template
+        if self.is_paginated(query_url):
+            page = 1
+            while page <= self.pages:
+                yield Url('{url}?page={page}'.format(url=query_url, page=page))
+                page += 1
+        else:
+            yield Url(query_url)
+
+    def process_urls(self):
+        self.log('Start data loading')
+        for url in self.get_urls():
+
+            self.process_url(url)
+
+            if self.req_count % self.save_count == 0:
+                self.update_db()
+                self.log('Process request {}'.format(self.req_count))
+
+            self.req_count += 1
+
+            if self.req_count == self.max_req_count:
+                self.log('Hit max request at {}'.format(self.req_count))
+                break
+        self.update_db()
+        self.log('Finish data loading')
